c5d3f1228e7caf77a3131317dae092feddb53a04
maxxng <maxngkaishen@gmail.com>
2021-01-08 12:51:53 +0800

    route added

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import connect from "src/db/connect";
-import usersRouter from "src/routes/userRouter";
 
-connect();
+import { usersRouter } from "src/routes/userRouter";
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 8080;
 
+connect();
+app.use(express.json());
 app.get("/", (req, res) => res.send("Hellooo world"));
 app.use('/users', usersRouter);
 
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,20 +1,23 @@
-import router from 'express.Router()';
+import { Router } from 'express';
 
 import { User } from "../models/User"
 
+const router = Router();
+
 router.route('/').get((req, res) => {
     User.find()
-        .then(User => res.json(User))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .then((User: any) => res.json(User))
+        .catch((err: any) => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
+    const username: String = req.body.username;
+    const password: String = req.body.password;
 
-    const newUser = new User({username});
+    const newUser = new User({username, password});
     newUser.save()
         .then(username => res.json(`New user: ${username} added.`))
         .catch(err => res.json('Error: ' + err));
 });
 
-module.exports = router;
+export const usersRouter = router;
