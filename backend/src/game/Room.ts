78f79d0ff434d1eb73385a88d86e386e9242e301
tankangliang <kang.tan@caperlab.com>
2021-01-09 03:22:13 +0800

    add java code

--- backend/src/game/Room.ts
+++ backend/src/game/Room.ts
@@ -4,7 +4,33 @@ import { Server } from "socket.io";
 import { Event } from "./protocols";
 import { Profile } from "../models/Profile";
 
-const text = `Lorem Ipsum is typesetting industry.`;
+const text = `package org.arpit.java2blog;
+ 
+import java.util.Scanner;
+ 
+public class FizzBuzzMain {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int n = s.nextInt();
+		System.out.println("The FizzBuzz numberswill be: ");
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) { //multiple of 3 & 5
+				System.out.print("FizzBuzz");
+			} else if (i % 3 == 0) { //multiple of 3
+				System.out.print("Fizz");
+			} else if (i % 5 == 0) { //multiple of 5
+				System.out.print("Buzz");
+			} else {
+				System.out.print(i);
+			}
+			System.out.print(" ");
+		}
+		s.close();
+	}
+}`;
 
 /**
  * Room represents the game room that users belong in.
--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function GamePage() {
   return (
@@ -19,98 +19,120 @@ function GamePage() {
   );
 }
 
-const text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+const text = `package org.arpit.java2blog;
+ 
+import java.util.Scanner;
+ 
+public class FizzBuzzMain {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int n = s.nextInt();
+		System.out.println("The FizzBuzz numberswill be: ");
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) { //multiple of 3 & 5
+				System.out.print("FizzBuzz");
+			} else if (i % 3 == 0) { //multiple of 3
+				System.out.print("Fizz");
+			} else if (i % 5 == 0) { //multiple of 5
+				System.out.print("Buzz");
+			} else {
+				System.out.print(i);
+			}
+			System.out.print(" ");
+		}
+		s.close();
+	}
+}`;
 
 function PracticeBox(props: any) {
   const { style } = props;
   const [ptr, setPtr] = useState(0);
 
-    const {
-        elapsedTime,
-        resetTimer,
-        startTimer,
-        stopTimer,
-    } = useStopwatch();
+  const { elapsedTime, resetTimer, startTimer, stopTimer } = useStopwatch();
 
+  return (
+    <div
+      className={"container"}
+      style={{ margin: style.margin }}
+      onKeyDown={(e) => {
+        if (ptr == 0) {
+          startTimer();
+        }
+        const isCorrectKeyPress =
+          e.key == text[ptr] || (e.key == "Enter" && text[ptr] == "\n");
 
-    return (
-        <div className={"container"} style={{ margin: style.margin }}
-             onKeyDown={(e) => {
-                 if (ptr == 0) {
-                     startTimer()
-                 }
-                 const isCorrectKeyPress =
-                     e.key == text[ptr] ||
-                     (e.key == "Enter" && text[ptr] == "\n");
+        if (isCorrectKeyPress) {
+          setPtr(ptr + 1);
+          if (ptr + 2 == text.length) stopTimer();
+        }
+      }}
+      tabIndex={0}
+    >
+      <p style={{ whiteSpace: "pre-wrap" }}>
+        <span style={{ color: "red" }}>{text.slice(0, ptr)}</span>
+        <span style={{ backgroundColor: "#FFE0AB" }}>{text[ptr]}</span>
+        <span>{text.slice(ptr + 1)}</span>
+      </p>
 
-                 if(isCorrectKeyPress) {
-                     setPtr(ptr + 1);
-                     if(ptr + 2 == text.length) stopTimer()
-
-                 }
-             }}
-             tabIndex={0}
-        >
-            <p style={{ whiteSpace: "pre-wrap"}}>
-
-                <span style={{color:"red"}}>{text.slice(0,ptr)}</span>
-                <span style={{backgroundColor:"#FFE0AB"}}>{text[ptr]}</span>
-                <span>{text.slice(ptr+1)}</span>
-            </p>
-
-            <p>---------------------------</p>
-            <p>Time  Elapsed : {elapsedTime.toFixed(1)}</p>
-            <p>WPM : {((text.slice(0,ptr).split(" ").length) / elapsedTime * 60).toFixed(0)}</p>
-            <p>---------------------------</p>
-            <button onClick={() => {
-                setPtr(0)
-                resetTimer()
-            }
-            }>Reset</button>
-        </div>
-    );
+      <p>---------------------------</p>
+      <p>Time Elapsed : {elapsedTime.toFixed(1)}</p>
+      <p>
+        WPM :{" "}
+        {((text.slice(0, ptr).split(" ").length / elapsedTime) * 60).toFixed(0)}
+      </p>
+      <p>---------------------------</p>
+      <button
+        onClick={() => {
+          setPtr(0);
+          resetTimer();
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export const useTimer = () => {
-    const [isRunning, setIsRunning] = useState(false);
-    const [elapsedTime, setElapsedTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
-    useEffect(
-        () => {
-            let interval: NodeJS.Timeout;
-            if (isRunning) {
-                interval = setInterval(
-                    () => setElapsedTime(prevElapsedTime => prevElapsedTime + 0.1),
-                    100
-                );
-            }
-            return () => clearInterval(interval);
-        },
-        [isRunning]
-    );
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isRunning) {
+      interval = setInterval(
+        () => setElapsedTime((prevElapsedTime) => prevElapsedTime + 0.1),
+        100
+      );
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
 
-    return {
-        isRunning,
-        setIsRunning,
-        elapsedTime,
-        setElapsedTime
-    };
+  return {
+    isRunning,
+    setIsRunning,
+    elapsedTime,
+    setElapsedTime,
+  };
 };
 export const useStopwatch = () => {
-    const { isRunning, setIsRunning, elapsedTime, setElapsedTime } = useTimer();
+  const { isRunning, setIsRunning, elapsedTime, setElapsedTime } = useTimer();
 
-    const handleReset = () => {
-        setIsRunning(false);
-        setElapsedTime(0);
-    };
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
 
-    return {
-        elapsedTime: elapsedTime,
-        resetTimer: () => handleReset(),
-        startTimer: () => setIsRunning(true),
-        stopTimer: () => setIsRunning(false),
-        // isRunning
-    };
+  return {
+    elapsedTime: elapsedTime,
+    resetTimer: () => handleReset(),
+    startTimer: () => setIsRunning(true),
+    stopTimer: () => setIsRunning(false),
+    // isRunning
+  };
 };
 
 export default GamePage;
