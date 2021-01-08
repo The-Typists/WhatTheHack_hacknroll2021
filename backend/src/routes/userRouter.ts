afaca484fe13de4a36e805345882cf1af2a3c584
maxxng <maxngkaishen@gmail.com>
2021-01-08 17:08:51 +0800

    add login route

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+//import crypto from "crypto";
 
 import { User } from "../models/User";
 
@@ -21,5 +22,41 @@ router.route("/add").post(async (req, res) => {
     res.json("Error: " + err);
   }
 });
+
+router.route("/:id").delete(async (req, res) => {
+    const username: string = req.body.username;
+    const password: string = req.body.password;
+
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json(`User, ${username} has been deleted successfully.`))
+        .catch((err: String) => res.json(`Error: ` + err));
+})
+
+router.route("/update/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((user: any) => {
+            user.username = req.body.username;
+            user.password = req.body.password;
+
+            user.save()
+                .then(() => res.json("update successful"))
+                .catch((err: any) => res.json('error: ' + err));
+        })
+})
+
+router.route("/login").post((req, res) => {
+    const username: string = req.body.username;
+    const password: string = req.body.password;
+
+    if(!req.body.username || !req.body.password){
+        res.json({message: "Please enter both username and password"});
+    } else {
+        User.verifyUser(username, password)
+            .then((user) => {
+                res.send(user.toJSON())
+            })
+            .catch((err) => res.json(err));
+    }
+})
 
 export const usersRouter = router;
