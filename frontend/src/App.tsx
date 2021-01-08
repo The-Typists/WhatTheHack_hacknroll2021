8f13e1d115467de6a2ec5475191060bd8da3ec5e
Jay <Nopenotj@gmail.com>
2021-01-08 11:26:28 +0800

    Structured Project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import {CodeBox} from "./components/CodeBox";
+import GamePage from "./pages/GamePage";
 
 function App() {
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100%",
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-            fontFamily: "Consola"
-        }}>
-            <div style={{marginBottom: 20, fontSize: 24}}>TypeRacer</div>
-            <CodeBox style={{margin: 10}}/>
-        </div>
+        <>
+            <GamePage />
+        </>
     );
 }
 
