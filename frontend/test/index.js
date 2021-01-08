cd20ea7225d4cbf12566dfdf320e94e7591001b4
Ritesh Kumar <ritesh@emerald.pink>
2021-01-08 11:04:47 +0800

    Front End init

---
+++ frontend/test/index.js
@@ -0,0 +1,46 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
+});
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', socket => {
+    // Event Handlers
+    socket.on('initialize', (x) => {
+        console.log("initalizing")
+        max = x
+    })
+    socket.on('disconnect', () => {
+        console.log("Disconnected!")
+        clearInterval(id)
+    })
+
+    // Logic
+    console.log('connect');
+    const opp = [0,0,0,0];
+    let max = 0;
+
+    id = setInterval(() => {
+        if(opp[0] < max) {
+            opp[0] += Math.floor(Math.random() * 10)
+            socket.emit('updatePos', opp)
+        }
+    },1000)
+
+});
+
+
+
+
+httpServer.listen(2105, () => {
+  console.log('go to http://localhost:2105');
+});
