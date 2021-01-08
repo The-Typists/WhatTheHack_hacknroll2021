ab90090dc1e8d5d78f26161787a1ffaf96f22edf
tankangliang <kang.tan@caperlab.com>
2021-01-08 13:17:23 +0800

    Update modelsgs

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import connect from "src/db/connect";
-
+import bodyParser from "body-parser";
 import { usersRouter } from "src/routes/userRouter";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT ? process.env.PORT : 8080;
 
 connect();
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hellooo world"));
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,23 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { User } from "../models/User"
+import { User } from "../models/User";
 
 const router = Router();
 
-router.route('/').get((req, res) => {
-    User.find()
-        .then((User: any) => res.json(User))
-        .catch((err: any) => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  User.find()
+    .then((User: any) => res.json(User))
+    .catch((err: any) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
-    const username: String = req.body.username;
-    const password: String = req.body.password;
-
-    const newUser = new User({username, password});
-    newUser.save()
-        .then(username => res.json(`New user: ${username} added.`))
-        .catch(err => res.json('Error: ' + err));
+router.route("/add").post((req, res) => {
+  console.log(req.body);
+  const username: String = req.body.username;
+  const password: String = req.body.password;
+  console.log(username);
+  console.log(password);
+  const newUser = new User({ username, password });
+  newUser
+    .save()
+    .then((username) => res.json(`New user: ${username} added.`))
+    .catch((err) => res.json("Error: " + err));
 });
 
 export const usersRouter = router;
