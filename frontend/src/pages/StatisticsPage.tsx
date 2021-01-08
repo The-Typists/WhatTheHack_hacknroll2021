f63f4af5ecaa7ea98f06edb4d65a0bf019f072b0
maxxng <maxngkaishen@gmail.com>
2021-01-09 03:36:04 +0800

    statistics variable not showing up

--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import {CodeBox} from "../components/CodeBox";
 
 function StatisticsPage() {
+    const [totalAttempts, setTotalAttempts] = useState("");
+    const id = JSON.parse(localStorage.getItem('user') || '{}').id
+    console.log(id);
+    useEffect(() => {
+        axios.get(`/profile/${id}`)
+            .then((res: any) => {
+                console.log(res);
+                setTotalAttempts(res.totalAttempts);
+            })
+            .catch((err: any) => {
+                console.log(err);
+            })
+        }
+    )
+
+
     return (
+        <>
         <div style={{
             display: "flex",
             flexDirection: "column",
@@ -13,7 +31,9 @@ function StatisticsPage() {
             fontFamily: "Consola"
         }}>
             <div style={{marginBottom: 20, fontSize: 24}}>Statistics</div>
+            <div>Total attempts: {totalAttempts}</div>
         </div>
+        </>
     );
 }
 
